28deced764f0bf1ab3057144924b914f794cbaab
eagrcode <elliot.robinson92@hotmail.com>
2023-02-05 16:48:52 +0000

    add style tweaks

--- src/components/Planning/Planning.jsx
+++ src/components/Planning/Planning.jsx
@@ -12,9 +12,11 @@ function Planning({ title }) {
           you by email, at the end of a phone, video calls and for extra meetings to help put your
           mind at ease, so you enjoy the process of creating your perfect day. Remember that it is
           your ceremony, so if you would like any changes or additions, just let me know. I want to
-          make sure that your script is just right for you. Before the big day, you may like a
-          rehearsal or you may not. We can discuss the individual benefits of this for your own
-          distinct ceremony during the planning process.
+          make sure that your script is just right for you.
+        </p>
+        <p className="section-content">
+          Before the big day, you may like a rehearsal or you may not. We can discuss the individual
+          benefits of this for your own distinct ceremony during the planning process.
         </p>
       </div>
     </section>
