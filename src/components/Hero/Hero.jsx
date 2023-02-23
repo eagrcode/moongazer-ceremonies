daecb232194df247587aea83822ac7004dee4ae7
eagrcode <elliot.robinson92@hotmail.com>
2023-02-23 15:35:16 +0000

    change nav + Hero color to accent color

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,17 +2,18 @@
 import "./Hero.css";
 
 import Hero1 from "../../assets/cake-table.jpg";
+import Hero2 from "../../assets/cake-table-bw.jpg";
 
 function Hero() {
   return (
     <div
       className="hero-container"
-      // style={{
-      //   backgroundImage: `linear-gradient(hsl(175, 40%, 95%, 0.1), hsl(175, 40%, 95%)), url(${Hero1})`,
-      // }}
       style={{
-        backgroundImage: `url(${Hero1})`,
+        backgroundImage: `linear-gradient(hsl(185, 60%, 97%, 0.4), hsl(185, 60%, 97%)), url(${Hero1})`,
       }}
+      // style={{
+      //   backgroundImage: `url(${Hero2})`,
+      // }}
     >
       <div className="hero-text-container">
         <h1 id="hero-text">Moon Gazer Ceremonies</h1>
