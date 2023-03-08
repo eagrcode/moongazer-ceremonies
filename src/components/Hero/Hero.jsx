9cc80432411e729b48ade78e7f9595ef48955b3f
eagrcode <elliot.robinson92@hotmail.com>
2023-03-08 15:35:13 +0000

    change Hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,16 +3,17 @@ import "./Hero.css";
 
 import Hero1 from "../../assets/cake-table.jpg";
 import Hero2 from "../../assets/cake-table-bw.jpg";
+import Hero3 from "../../assets/white-rose.jpg";
 
 function Hero() {
   return (
     <div
       className="hero-container"
-      style={{
-        backgroundImage: `linear-gradient(hsl(185, 60%, 97%, 0.4), hsl(185, 60%, 97%)), url(${Hero1})`,
-      }}
       // style={{
-      //   backgroundImage: `url(${Hero2})`,
+      //   backgroundImage: `linear-gradient(hsl(185, 60%, 97%, 0.4), hsl(185, 60%, 97%)), url(${Hero3})`,
+      // }}
+      // style={{
+      //   backgroundImage: `url(${Hero3})`,
       // }}
     >
       <div className="hero-text-container">
