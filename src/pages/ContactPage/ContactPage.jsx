a7731cf4513f8458ed2a9e69acee9144004a8060
eagrcode <elliot.robinson92@hotmail.com>
2023-03-14 17:43:48 +0000

    add stateful form test

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,12 +1,35 @@
 // styles
 import "./ContactPage.css";
 
-// components
-import { MainBtn } from "../../components";
-
+// icons
 import { MdEmail } from "react-icons/md";
 
+// react
+import { useState } from "react";
+
 function ContactPage() {
+  // state
+  const [formData, setFormData] = useState({ firstName: "", lastName: "", email: "", message: "" });
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  };
+
+  const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contactForm", formData }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+    e.preventDefault();
+  };
+
   return (
     <section className="content-section contact">
       <h1>Contact Me</h1>
@@ -21,12 +44,38 @@ function ContactPage() {
         </div>
       </div>
 
-      <form name="contact" method="POST" data-netlify="true" action="/success">
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="first-Name" placeholder="First Name" required minLength="2" />
-        <input type="text" name="last-Name" placeholder="Last Name" required minLength="2" />
-        <input type="email" name="email" placeholder="Email Adress" minLength="5" required />
+      <form name="contact" onSubmit={handleSubmit}>
+        {/* <input type="hidden" name="form-name" value="contact" /> */}
+        <input
+          onChange={handleChange}
+          value={formData.firstName}
+          type="text"
+          name="first-Name"
+          placeholder="First Name"
+          required
+          minLength="2"
+        />
+        <input
+          onChange={handleChange}
+          value={formData.lastName}
+          type="text"
+          name="last-Name"
+          placeholder="Last Name"
+          required
+          minLength="2"
+        />
+        <input
+          onChange={handleChange}
+          value={formData.email}
+          type="email"
+          name="email"
+          placeholder="Email Adress"
+          minLength="5"
+          required
+        />
         <textarea
+          onChange={handleChange}
+          value={formData.message}
           name="message"
           placeholder="Message"
           minLength="10"
