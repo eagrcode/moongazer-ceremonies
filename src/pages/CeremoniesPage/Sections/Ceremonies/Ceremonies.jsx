30a5b88d828f67d718f35c0ec07c099be714b75f
eagrcode <elliot.robinson92@hotmail.com>
2023-03-28 14:16:14 +0100

    add funeral img

--- src/pages/CeremoniesPage/Sections/Ceremonies/Ceremonies.jsx
+++ src/pages/CeremoniesPage/Sections/Ceremonies/Ceremonies.jsx
@@ -5,6 +5,7 @@ import Wedding from "../../../../assets/wedding.jpg";
 import Renewal from "../../../../assets/renewal.jpg";
 import Commitment from "../../../../assets/commitment-ceremony.jpg";
 import Naming from "../../../../assets/naming-ceremony.jpg";
+import Funeral from "../../../../assets/funeral.jpg";
 
 // Styles
 import "./Ceremonies.css";
@@ -17,7 +18,7 @@ function Ceremonies() {
         <CeremonyCard title={"Vow Renewal"} img={Renewal} path={"/renewal"} />
         <CeremonyCard title={"Commitment"} img={Commitment} path={"/unity"} />
         <CeremonyCard title={"Naming"} img={Naming} path={"/naming"} />
-        <CeremonyCard title={"Funeral"} img={Naming} path={"/funeral"} />
+        <CeremonyCard title={"Funeral"} img={Funeral} path={"/funeral"} />
       </div>
     </section>
   );
