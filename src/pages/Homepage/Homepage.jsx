d2bf279fc4d2ab87a598b9a00eb7539dd7fddaf6
eagrcode <elliot.robinson92@hotmail.com>
2023-09-01 11:05:27 +0100

    fix #2

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -18,6 +18,7 @@ function Homepage() {
   return (
     <>
       <Hero />
+
       <About title={"About Me"} />
       <ImageContainer img={jump} alt="wedding-men-jump" />
       <Why title={"Why Choose A Celebrant?"} />
